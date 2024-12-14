7d2304c7069649a1a9f2cf255dd9a31b5b8781f0
isaackogan <isaacikogan@gmail.com>
2024-12-14 16:50:34 -0500

    Update docker-compose.yml

--- MinecraftServer/build.sh
+++ MinecraftServer/build.sh
@@ -5,4 +5,4 @@ docker buildx use retard-uit
 docker buildx build --push \
 --platform linux/amd64,linux/arm64 \
 --tag isaackogan/mineplex-arcade-server:latest \
---tag isaackogan/mineplex-arcade-server:v0.0.1 .
+--tag isaackogan/mineplex-arcade-server:v0.0.2 .
